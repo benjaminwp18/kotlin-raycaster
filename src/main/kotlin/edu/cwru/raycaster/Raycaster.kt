89fd84960e3e80026629f8e2c01bd6771bcdb559
benjaminwp18 <90342856+benjaminwp18@users.noreply.github.com>
2025-03-12 22:44:18 -0400

    Basic top down player & map

--- src/main/kotlin/edu/cwru/raycaster/Raycaster.kt
+++ src/main/kotlin/edu/cwru/raycaster/Raycaster.kt
@@ -13,70 +13,150 @@ import javafx.stage.Stage
 import java.nio.IntBuffer
 
 val KEYS = listOf('w', 'a', 's', 'd', 'q', 'e')
-const val NS_TO_S = 1_000_000_000.0
-const val SIXTY_FPS_DELTA_NS = NS_TO_S / 60.0
+
+const val NS_IN_S = 1_000_000_000.0
+
+const val PX_PER_BLOCK = 50
+
+const val PLAYER_MOVE_RATE = 3.0  // blocks / sec
+const val PLAYER_RADIUS_BLOCKS = 0.5
+const val PLAYER_RADIUS_PX = (PLAYER_RADIUS_BLOCKS * PX_PER_BLOCK).toInt()
+
+val BLOCK_COLORS = mapOf(
+    0 to Color.WHITE,
+    1 to Color.BLACK,
+).withDefault { Color.PURPLE }  // Noticeable error color
+
+private val MAP = arrayOf(
+    intArrayOf(1, 1, 1, 1, 1, 1),
+    intArrayOf(1, 0, 0, 0, 0, 1),
+    intArrayOf(1, 0, 0, 0, 0, 1),
+    intArrayOf(1, 0, 0, 0, 0, 1),
+    intArrayOf(1, 0, 0, 0, 0, 1),
+    intArrayOf(1, 1, 1, 1, 1, 1)
+)
+
+val MAP_WIDTH_BLOCKS = MAP[0].size
+val MAP_HEIGHT_BLOCKS = MAP.size
+val MAP_WIDTH_PX = MAP_WIDTH_BLOCKS * PX_PER_BLOCK
+val MAP_HEIGHT_PX = MAP_HEIGHT_BLOCKS * PX_PER_BLOCK
 
 fun Double.format(scale: Int) = "%.${scale}f".format(this)
+
+data class Vec2<T : Comparable<T>>(val x: T, val y: T)
+data class MutableVec2<T : Comparable<T>>(var x: T, var y: T) {
+    fun clamp(xMin: T, xMax: T, yMin: T, yMax: T) {
+        x = minOf(maxOf(x, xMin), xMax)
+        y = minOf(maxOf(y, yMin), yMax)
+    }
+}
 
 class Raycaster : Application() {
     private val keyMap: MutableMap<Char, Boolean> = KEYS.associateWith { false }.toMutableMap().withDefault { false }
     private var prevFrameTime = 0L
+    private val playerPosition = MutableVec2(1.0, 1.0)
 
     override fun start(primaryStage: Stage) {
-        val frameRateLabel = Label("??.??")
+        val frameRateLabel = Label("No FPS data")
 
         primaryStage.title = "Kotlin Raycaster"
 
-        val imageView = ImageCanvas(100, 100)
+        val topDownView = ImageCanvas(MAP_WIDTH_PX, MAP_HEIGHT_PX)
 
         val root = VBox()
         root.children.add(frameRateLabel)
-        root.children.add(imageView)
+        root.children.add(topDownView)
 
-        primaryStage.scene = Scene(root, 300.0, 250.0)
+        primaryStage.scene = Scene(root, 500.0, 500.0)
 
         primaryStage.scene.setOnKeyPressed {
-            println("Pressed '${it.text}'")
-            val keyChar = it.text[0]
-            if (keyChar in keyMap.keys) {
-                keyMap[keyChar] = true
+            if (it.text.length == 1) {
+                val keyChar = it.text[0]
+                if (keyChar in keyMap.keys) {
+                    keyMap[keyChar] = true
+                }
             }
-            println(keyMap)
         }
         primaryStage.scene.setOnKeyReleased {
-            println("Released '${it.text}'")
-            val keyChar = it.text[0]
-            if (keyChar in keyMap.keys) {
-                keyMap[keyChar] = false
+            if (it.text.length == 1) {
+                val keyChar = it.text[0]
+                if (keyChar in keyMap.keys) {
+                    keyMap[keyChar] = false
+                }
             }
-            println(keyMap)
         }
 
         primaryStage.show()
 
         object : AnimationTimer() {
             override fun handle(now: Long) {
-                val deltaNanoS = now - prevFrameTime
+                // Time delta from previous frame in seconds; multiply rates by this to deal with variable FPS
+                val deltaSec = (now - prevFrameTime) / NS_IN_S
                 // Instantaneous frame rate
-                val frameRate = 1_000_000_000.0 / deltaNanoS
-                // Delta relative to 60 FPS; multiply rates by relativeDelta to account for FPS fluctuations
-                val relativeDelta = deltaNanoS / SIXTY_FPS_DELTA_NS
-                frameRateLabel.text = "FPS: ${frameRate.format(2)}\nRδ:  ${relativeDelta.format(2)}"
+                val frameRate = 1.0 / deltaSec
+                frameRateLabel.text = "FPS: ${frameRate.format(2)}\nδS: ${deltaSec.format(2)}"
                 prevFrameTime = now
 
-                imageView.prepRect(10, 20, 30, 30, Color(10, 10, (0..255).random(), 255))
-                imageView.redraw()
+                if (keyMap.getValue('w')) {
+                    playerPosition.y -= PLAYER_MOVE_RATE * deltaSec
+                }
+                if (keyMap.getValue('s')) {
+                    playerPosition.y += PLAYER_MOVE_RATE * deltaSec
+                }
+                if (keyMap.getValue('d')) {
+                    playerPosition.x += PLAYER_MOVE_RATE * deltaSec
+                }
+                if (keyMap.getValue('a')) {
+                    playerPosition.x -= PLAYER_MOVE_RATE * deltaSec
+                }
+
+                // Don't leave the map
+                playerPosition.clamp(
+                    PLAYER_RADIUS_BLOCKS, MAP_WIDTH_BLOCKS - PLAYER_RADIUS_BLOCKS,
+                    PLAYER_RADIUS_BLOCKS, MAP_HEIGHT_BLOCKS - PLAYER_RADIUS_BLOCKS
+                )
+
+                for ((r, row) in MAP.withIndex()) {
+                    for ((c, block) in row.withIndex()) {
+                        topDownView.prepRect(
+                            r * PX_PER_BLOCK, c * PX_PER_BLOCK,
+                            PX_PER_BLOCK, PX_PER_BLOCK,
+                            BLOCK_COLORS.getValue(block)
+                        )
+                    }
+                }
+
+                topDownView.prepRect(
+                    (playerPosition.x * PX_PER_BLOCK).toInt() - PLAYER_RADIUS_PX,
+                    (playerPosition.y * PX_PER_BLOCK).toInt() - PLAYER_RADIUS_PX,
+                    PLAYER_RADIUS_PX * 2, PLAYER_RADIUS_PX * 2,
+                    Color.RED
+                )
+
+                topDownView.redraw()
             }
         }.start()
     }
 }
 
-// TODO: replace with extension function on JavaFX Color?
+// TODO: replace with extension function on JavaFX Color? Maybe not cause it stores everything as floats?
 data class Color(val r: Int, val g: Int, val b: Int, val a: Int) {
     fun toInt() = (a shl 24) or (r shl 16) or (g shl 8) or b
-}
 
-data class ArrayCoords(val x: Int, val y: Int)
+    companion object {
+        fun rgb(r: Int, g: Int, b: Int) = Color(r, g, b, 255)
+        fun rgba(r: Int, g: Int, b: Int, a: Int) = Color(r, g, b, a)
+
+        val RED = Color.rgb(255, 0, 0)
+        val GREEN = Color.rgb(0, 255, 0)
+        val BLUE = Color.rgb(0, 0, 255)
+        val PURPLE = Color.rgb(255, 0, 255)
+        val AQUA = Color.rgb(0, 255, 255)
+        val YELLOW = Color.rgb(255, 255, 0)
+        val BLACK = Color.rgb(0, 0, 0)
+        val WHITE = Color.rgb(255, 255, 255)
+    }
+}
 
 class ImageCanvas private constructor(
     private val pixelBuffer: PixelBuffer<IntBuffer>
@@ -88,7 +168,7 @@ class ImageCanvas private constructor(
     private val height: Int = pixelBuffer.height
 
     private fun coordsToIndex(x: Int, y: Int) = (x % width) + (y * width)
-    private fun indexToCoords(i: Int) = ArrayCoords(i % width, i / width)
+    private fun indexToCoords(i: Int) = Vec2(i % width, i / width)
 
     fun prepPixel(x: Int, y: Int, color: Color) {
         pixels[coordsToIndex(x, y)] = color.toInt()
@@ -103,7 +183,7 @@ class ImageCanvas private constructor(
     }
 
     fun redraw() {
-        // tell the buffer that the entire area needs redrawing
+        // Tell the buffer that the entire area needs redrawing
         // TODO: keep track of areas that have been "prepped" and only redraw those?
         pixelBuffer.updateBuffer { b: PixelBuffer<IntBuffer>? -> null }
     }
