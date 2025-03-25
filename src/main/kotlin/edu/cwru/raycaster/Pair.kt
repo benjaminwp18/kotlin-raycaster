063a2bb891eb022b269cb85c217e7dd0fb7ab307
benjaminwp18 <90342856+benjaminwp18@users.noreply.github.com>
2025-03-25 12:49:09 -0400

    Cleanup more & add destructuring

--- src/main/kotlin/edu/cwru/raycaster/Pair.kt
+++ src/main/kotlin/edu/cwru/raycaster/Pair.kt
@@ -7,23 +7,13 @@ val <A, B> Pair<A, B>.size: Int
 
 private operator fun <T> Pair<T, T>.get(index: Int): T =
     when (index) {
-      0 -> this.first
-      1 -> this.second
-      else -> throw InvalidParameterException("index was not 1 or 2")
+      0 -> first
+      1 -> second
+      else -> throw InvalidParameterException("Index into a Pair must be 0 or 1, got $index")
     }
 
-class PairIterator<T>(private val p: Pair<T, T>) : Iterator<T> {
+operator fun <T> Pair<T, T>.iterator() = object : Iterator<T> {
   private var index = 0
-
-  override fun hasNext(): Boolean {
-    return index < p.size
-  }
-
-  override fun next(): T {
-    return p[index++]
-  }
-}
-
-operator fun <T> Pair<T, T>.iterator(): Iterator<T> {
-  return PairIterator(this)
+  override fun hasNext() = index < size
+  override fun next() = get(index++)
 }
